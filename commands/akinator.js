7b81aabb060852238594a097961c7d17a1113622
Ivan <ivanrizqy.ir@gmail.com>
2023-11-10 22:19:43 +0700

    Lint setup

--- commands/akinator.js
+++ commands/akinator.js
@@ -1,141 +1,138 @@
-import { Aki } from "aki-api";
-import { ActionRowBuilder, ButtonBuilder, ButtonStyle, ComponentType, EmbedBuilder, SlashCommandBuilder } from "discord.js";
-import { AKINATOR_REGION, AKINATOR_CHILD_MODE, AKINATOR_PROXY, AKINATOR_MAX_RESPONSE_MINUTE, AKINATOR_WIN_EVERY_STEP, AKINATOR_WIN_PROGRESS, AKINATOR_MAX_GUESS_COUNT } from "../config.js";
+import { Aki } from 'aki-api'
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, SlashCommandBuilder } from 'discord.js'
+import { AKINATOR_REGION, AKINATOR_CHILD_MODE, AKINATOR_PROXY, AKINATOR_MAX_RESPONSE_MINUTE, AKINATOR_WIN_EVERY_STEP, AKINATOR_WIN_PROGRESS, AKINATOR_MAX_GUESS_COUNT } from '../config.js'
 
 export default {
-    data: new SlashCommandBuilder()
-        .setName('akinator')
-        .setDescription('Memulai akinator'),
-    async execute(interaction) {
-        await interaction.reply('Pikirkan sebuah tokoh atau karakter untuk saya tebak...');
+  data: new SlashCommandBuilder()
+    .setName('akinator')
+    .setDescription('Memulai akinator'),
+  async execute (interaction) {
+    await interaction.reply('Pikirkan sebuah tokoh atau karakter untuk saya tebak...')
 
-        const aki = new Aki({ region: AKINATOR_REGION, childMode: AKINATOR_CHILD_MODE, proxy: AKINATOR_PROXY });
+    const aki = new Aki({ region: AKINATOR_REGION, childMode: AKINATOR_CHILD_MODE, proxy: AKINATOR_PROXY })
 
-        const generateEmbed = (akiQuestion, akiProgress, akiCurrentStep) => (new EmbedBuilder())
-            .setTitle(akiQuestion)
-            .setFooter({ text: `Progress: ${akiProgress}%, Step: ${akiCurrentStep + 1}` });
+    const generateEmbed = (akiQuestion, akiProgress, akiCurrentStep) => (new EmbedBuilder())
+      .setTitle(akiQuestion)
+      .setFooter({ text: `Progress: ${akiProgress}%, Step: ${akiCurrentStep + 1}` })
 
-        const generateComponents = (akiCurrentStep) => {
-            const answerButtons = tempAki.answers.map((answer, index) => (new ButtonBuilder).setCustomId(index.toString())
-                .setLabel(answer)
-                .setStyle(ButtonStyle.Primary));
+    const generateComponents = (akiCurrentStep) => {
+      const answerButtons = tempAki.answers.map((answer, index) => (new ButtonBuilder()).setCustomId(index.toString())
+        .setLabel(answer)
+        .setStyle(ButtonStyle.Primary))
 
-            const backButton = (new ButtonBuilder).setCustomId('back')
-                .setLabel('Kembali')
-                .setStyle(ButtonStyle.Secondary);
+      const backButton = (new ButtonBuilder()).setCustomId('back')
+        .setLabel('Kembali')
+        .setStyle(ButtonStyle.Secondary)
 
-            const stopButton = (new ButtonBuilder).setCustomId('stop')
-                .setLabel('Berhenti')
-                .setStyle(ButtonStyle.Danger);
+      const stopButton = (new ButtonBuilder()).setCustomId('stop')
+        .setLabel('Berhenti')
+        .setStyle(ButtonStyle.Danger)
 
-            const answerRow = (new ActionRowBuilder()).addComponents(...answerButtons);
-            const actionRow = new ActionRowBuilder();
+      const answerRow = (new ActionRowBuilder()).addComponents(...answerButtons)
+      const actionRow = new ActionRowBuilder()
 
-            if (akiCurrentStep !== 0) {
-                actionRow.addComponents(backButton);
-            }
+      if (akiCurrentStep !== 0) {
+        actionRow.addComponents(backButton)
+      }
 
-            actionRow.addComponents(stopButton);
+      actionRow.addComponents(stopButton)
 
-            return [answerRow, actionRow];
-        };
+      return [answerRow, actionRow]
+    }
 
-        let tempAki = await aki.start();
-        let guessCounter = 0;
+    let tempAki = await aki.start()
+    let guessCounter = 0
 
-        while (true) {
+    while (true) {
+      const response = await interaction.editReply({
+        content: '',
+        embeds: [generateEmbed(tempAki.question, aki.progress, aki.currentStep)],
+        components: generateComponents(aki.currentStep)
+      })
 
-            const response = await interaction.editReply({
-                content: '',
-                embeds: [generateEmbed(tempAki.question, aki.progress, aki.currentStep)],
-                components: generateComponents(aki.currentStep),
-            });
+      try {
+        const confirmation = await response.awaitMessageComponent({ filter: i => i.user.id === interaction.user.id, time: AKINATOR_MAX_RESPONSE_MINUTE * 60000 })
 
-            try {
-                const confirmation = await response.awaitMessageComponent({ filter: i => i.user.id === interaction.user.id, time: AKINATOR_MAX_RESPONSE_MINUTE * 60000 });
+        if (confirmation.customId === 'stop') {
+          await confirmation.update({
+            content: 'Akinator dihentikan',
+            embeds: [],
+            components: []
+          })
 
-                if (confirmation.customId === 'stop') {
-                    await confirmation.update({
-                        content: 'Akinator dihentikan',
-                        embeds: [],
-                        components: [],
-                    });
+          break
+        } else if (confirmation.customId === 'back') {
+          tempAki = await aki.back()
+        } else {
+          const answer = +confirmation.customId
 
-                    break;
-                } else if (confirmation.customId === 'back') {
-                    tempAki = await aki.back();
-                } else {
-                    const answer = +confirmation.customId;
-
-                    tempAki = await aki.step(answer);
-                }
-
-                await confirmation.deferUpdate();
-            } catch (e) {
-                await interaction.editReply({ content: `Confirmation not received within ${AKINATOR_MAX_RESPONSE_MINUTE} minute, cancelling`, components: [] });
-            }
-
-            if (aki.progress >= AKINATOR_WIN_PROGRESS || aki.currentStep % AKINATOR_WIN_EVERY_STEP === 0) {
-
-                await interaction.editReply({
-                    content: 'Tunggu sebentar, saya sedang menebak...',
-                    embeds: [],
-                    components: [],
-                });
-
-                const winAki = await aki.win();
-                const firstGuess = winAki.guesses[0];
-                guessCounter++;
-
-                const guessEmbed = new EmbedBuilder().setTitle(firstGuess.name)
-                    .setDescription(firstGuess.description)
-                    .setImage(firstGuess.absolute_picture_path);
-
-                const winResponse = await interaction.editReply({
-                    content: 'Apakah ini yang kamu maksud?',
-                    embeds: [guessEmbed],
-                    components: [
-                        (new ActionRowBuilder()).addComponents(
-                            (new ButtonBuilder()).setCustomId('yes')
-                                .setLabel('Iya')
-                                .setStyle(ButtonStyle.Primary),
-                            (new ButtonBuilder()).setCustomId('no')
-                                .setLabel('Tidak')
-                                .setStyle(ButtonStyle.Secondary)
-                        ),
-                    ]
-                });
-
-                try {
-                    const winConfirmation = await winResponse.awaitMessageComponent({ filter: i => i.user.id === interaction.user.id, time: AKINATOR_MAX_RESPONSE_MINUTE * 60000 });
-
-                    if (winConfirmation.customId === 'yes') {
-                        await winConfirmation.update({
-                            content: 'Baik, berarti tebakan saya benar.',
-                            embeds: [guessCounter.setFooter(`Jumlah tebakan: ${guessCounter}`)],
-                            components: [],
-                        })
-                        break;
-                    }
-
-                    if (winConfirmation.customId === 'no') {
-                        await winConfirmation.deferUpdate();
-                    }
-
-                } catch (e) {
-                    await interaction.editReply({ content: `Guess confirmation not received within ${AKINATOR_MAX_RESPONSE_MINUTE} minute, cancelling`, components: [] });
-                }
-            }
-
-            if (guessCounter >= AKINATOR_MAX_GUESS_COUNT) {
-                await interaction.editReply({
-                    content: 'Baiklah, saya menyerah 🏳️🏳️🏳️. Saya tidak tahu lagi apa yang sedang kamu pikirkan.',
-                    embeds: [],
-                    components: [],
-                });
-
-                break;
-            }
+          tempAki = await aki.step(answer)
         }
-    },
-};
+
+        await confirmation.deferUpdate()
+      } catch (e) {
+        await interaction.editReply({ content: `Confirmation not received within ${AKINATOR_MAX_RESPONSE_MINUTE} minute, cancelling`, components: [] })
+      }
+
+      if (aki.progress >= AKINATOR_WIN_PROGRESS || aki.currentStep % AKINATOR_WIN_EVERY_STEP === 0) {
+        await interaction.editReply({
+          content: 'Tunggu sebentar, saya sedang menebak...',
+          embeds: [],
+          components: []
+        })
+
+        const winAki = await aki.win()
+        const firstGuess = winAki.guesses[0]
+        guessCounter++
+
+        const guessEmbed = new EmbedBuilder().setTitle(firstGuess.name)
+          .setDescription(firstGuess.description)
+          .setImage(firstGuess.absolute_picture_path)
+
+        const winResponse = await interaction.editReply({
+          content: 'Apakah ini yang kamu maksud?',
+          embeds: [guessEmbed],
+          components: [
+            (new ActionRowBuilder()).addComponents(
+              (new ButtonBuilder()).setCustomId('yes')
+                .setLabel('Iya')
+                .setStyle(ButtonStyle.Primary),
+              (new ButtonBuilder()).setCustomId('no')
+                .setLabel('Tidak')
+                .setStyle(ButtonStyle.Secondary)
+            )
+          ]
+        })
+
+        try {
+          const winConfirmation = await winResponse.awaitMessageComponent({ filter: i => i.user.id === interaction.user.id, time: AKINATOR_MAX_RESPONSE_MINUTE * 60000 })
+
+          if (winConfirmation.customId === 'yes') {
+            await winConfirmation.update({
+              content: 'Baik, berarti tebakan saya benar.',
+              embeds: [guessCounter.setFooter(`Jumlah tebakan: ${guessCounter}`)],
+              components: []
+            })
+            break
+          }
+
+          if (winConfirmation.customId === 'no') {
+            await winConfirmation.deferUpdate()
+          }
+        } catch (e) {
+          await interaction.editReply({ content: `Guess confirmation not received within ${AKINATOR_MAX_RESPONSE_MINUTE} minute, cancelling`, components: [] })
+        }
+      }
+
+      if (guessCounter >= AKINATOR_MAX_GUESS_COUNT) {
+        await interaction.editReply({
+          content: 'Baiklah, saya menyerah 🏳️🏳️🏳️. Saya tidak tahu lagi apa yang sedang kamu pikirkan.',
+          embeds: [],
+          components: []
+        })
+
+        break
+      }
+    }
+  }
+}
--- index.js
+++ index.js
@@ -1,75 +1,75 @@
-import fs from 'node:fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { Client, Collection, Events, GatewayIntentBits, REST, Routes } from 'discord.js';
-import { DISCORD_APPLICATION_ID, DISCORD_TOKEN } from './config.js';
+import fs from 'node:fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { Client, Collection, Events, GatewayIntentBits, REST, Routes } from 'discord.js'
+import { DISCORD_APPLICATION_ID, DISCORD_TOKEN } from './config.js'
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 
-client.commands = new Collection();
+client.commands = new Collection()
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const commandPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandPath).filter(file => file.endsWith('.js'));
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const commandPath = path.join(__dirname, 'commands')
+const commandFiles = fs.readdirSync(commandPath).filter(file => file.endsWith('.js'))
 
 // Register commands
 for (const file of commandFiles) {
-	const filePath = path.join(commandPath, file);
-	const command = (await import(filePath)).default;
+  const filePath = path.join(commandPath, file)
+  const command = (await import(filePath)).default
 
-	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
-	} else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-	}
+  if ('data' in command && 'execute' in command) {
+    client.commands.set(command.data.name, command)
+  } else {
+    console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`)
+  }
 }
 
 const rest = new REST().setToken(DISCORD_TOKEN);
 
 // Deploy commands
 (async () => {
-	const commands = client.commands.map(command => command.data);
+  const commands = client.commands.map(command => command.data)
 
-	try {
-		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+  try {
+    console.log(`Started refreshing ${commands.length} application (/) commands.`)
 
-		const data = await rest.put(
-			Routes.applicationCommands(DISCORD_APPLICATION_ID),
-			{ body: commands },
-		);
+    const data = await rest.put(
+      Routes.applicationCommands(DISCORD_APPLICATION_ID),
+      { body: commands }
+    )
 
-		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-	} catch (error) {
-		console.error(error);
-	}
-})();
+    console.log(`Successfully reloaded ${data.length} application (/) commands.`)
+  } catch (error) {
+    console.error(error)
+  }
+})()
 
 client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
-});
+  console.log(`Ready! Logged in as ${c.user.tag}`)
+})
 
 // Command handling listener
 client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
+  if (!interaction.isChatInputCommand()) return
 
-	const command = interaction.client.commands.get(interaction.commandName);
+  const command = interaction.client.commands.get(interaction.commandName)
 
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`)
+    return
+  }
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-	}
-});
+  try {
+    await command.execute(interaction)
+  } catch (error) {
+    console.error(error)
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true })
+    } else {
+      await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
+    }
+  }
+})
 
 // Start the bot
-client.login(DISCORD_TOKEN);
+client.login(DISCORD_TOKEN)
